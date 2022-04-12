f8a901bd3305fed2425e53f36a9a935af9284ad3
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-12 21:42:00 +0600

    add too header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+
+      </Routes>
 
     </div>
   );
---
+++ src/Login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+   return (
+      <div>
+         <div>
+            <input type="text" name="" />
+            <input type="email" name="" id="" />
+            <input type="password" name="" id="" />
+         </div>
+      </div>
+   );
+};
+
+export default Login;
